42734ad27c23978a2b8ed832f1cc00cb0f70081d
Blake Peterman <peterman.blake@gmail.com>
2013-12-18 01:57:05 -0500

    Removed comments and whitespace

--- tester.cpp
+++ tester.cpp
@@ -44,10 +44,6 @@ void handle_decoding(std::string s);
 void handle_encoding(std::string s);
 
 
-
-
-
-
 //
 //MAIN PROGRAM BEGIN
 //
@@ -90,121 +86,7 @@ int main(void)
 	}
 
 #pragma region old_test_stuff
-	//cout << "TESTING BINARY SEARCH TREE METHODS" << endl;
-	//tree<string> t;
-	//tree<string> t2;
-	//t2.insert("40");
-	//t2.insert("50");
-	//t2.insert("45");
-	//t2.insert("60");
-	//t2.insert("55");
-	//t2.insert("47");
-	//t2.insert("30");
-	//t2.insert("35");
-	//t2.insert("32");
-	//
-	//// test SMALLEST
-	//cout << "\nTESTING SMALLEST METHOD" << endl;
-	//
-	//try {
-	//	t.smallest();
-	//	cout << "\tSMALLEST for empty tree does NOT throw domain error exception. (INCORRECT)" << endl;
-	//}
-	//catch (domain_error &de) {
-	//	cout << "\tSMALLEST for empty tree throws domain error exception. (CORRECT)" << endl;
-	//}
-	//cout << "\tBuilding test tree." << endl;
-	//
-	//t2.pre_print();
-	//cout << endl;
-	//if (t2.smallest() == "30")
-	//	cout << "\tSmallest element of tree == \"30\". (CORRECT)" << endl;
-	//else
-	//	cout << "\tSmallest element of tree != \"30\". (INCORRECT)" << endl;
-	//
-	//// test LARGEST
-	//cout << "\nTESTING LARGEST METHOD" << endl;
-	//
-	//try {
-	//	t.largest();
-	//	cout << "\tLARGEST for empty tree does NOT throw domain error exception. (INCORRECT)" << endl;
-	//}
-	//catch (domain_error &de) {
-	//	cout << "\tLARGEST for empty tree throws domain error exception. (CORRECT)" << endl;
-	//}
-	//cout << "\tBuilding test tree." << endl;
-	//if (t2.largest() == "60")
-	//	cout << "\tLargest element of tree == \"60\". (CORRECT)" << endl;
-	//else
-	//	cout << "\tLargest element of tree != \"60\". (INCORRECT)" << endl;
-	//	
-	//// test IN_SUM
-	//cout << "\nTESTING IN_SUM METHOD" << endl;
-	//
-	//if (t.in_sum() == "")
-	//	cout << "\tIn-order Sum of empty tree of strings is empty string. (CORRECT)" << endl;
-	//else
-	//	cout << "\tIn-order Sum of empty tree of strings is NOT empty string. (INCORRECT)" << endl;
-	//cout << "\tBuilding test tree." << endl;
-	//if (t2.in_sum() == "303235404547505560")
-	//	cout << "\tIn-order Sum of tree of strings == \"303235404547505560\". (CORRECT)" << endl;
-	//else
-	//	cout << "\tIn-order Sum of tree of strings != \"303235404547505560\". (INCORRECT)" << endl;
-	////cout << t2.in_sum() << endl;
-	//
-	//// test POST_SUM
-	//cout << "\nTESTING POST_SUM METHOD" << endl;
-	//
-	//if (t.post_sum() == "")
-	//	cout << "\tPost-order Sum of empty tree of strings is empty string. (CORRECT)" << endl;
-	//else
-	//	cout << "\tPost-order Sum of empty tree of strings is NOT empty string. (INCORRECT)" << endl;
-	//cout << "\tBuilding test tree." << endl;
-	//if (t2.post_sum() == "323530474555605040")
-	//	cout << "\tPost-order Sum of tree of strings == \"323530474555605040\". (CORRECT)" << endl;
-	//else
-	//	cout << "\tPost-order Sum of tree of strings != \"323530474555605040\". (INCORRECT)" << endl;
-	//
-	///*t2.~tree();
-	//try{
-	//	t2.smallest();
-	//}
-	//catch (domain_error &de) {
-	//	cout << "\tSMALLEST for empty tree throws domain error exception. (CORRECT)" << endl;
-	//}*/
-
-	//// test copy constructor
-	//cout << "\nTESTING COPY CONSTRUCTOR" << endl;
-	//tree<string> t3;
-	//t3.insert("40");
-	//t3.insert("50");
-	//t3.insert("30");
-	//
-	//tree<string> t4 = t3;
-	//t3.insert("20");
-	//t4.insert("60");
-	//if ((t3.smallest() == "20") && t3.contains("40") && t3.contains("30") && t3.contains("50") && !t3.contains("60") &&
-	//	(t4.largest() == "60") && t4.contains("40") && t4.contains("30") && t4.contains("50") && !t4.contains("20"))
-	//	cout << "Copy constructor corrrectly made tree t4 from tree t3. (CORRECT)" << endl;
-	//else
-	//	cout << "Copy constructor incorrrectly made tree t4 from tree t3. (INCORRECT)" << endl;
-	//
-	//// test assignment operator
-	//cout << "\nTESTING ASSIGNMENT OPERATOR" << endl;
-	//tree<string> t5;
-	//t5.insert("40");
-	//t5.insert("50");
-	//t5.insert("30");
-	//
-	//tree<string> t6;
-	//t6 = t5;
-	//t5.insert("20");
-	//t6.insert("60");
-	//if ((t5.smallest() == "20") && t5.contains("40") && t5.contains("30") && t5.contains("50") && !t5.contains("60") &&
-	//	(t6.largest() == "60") && t6.contains("40") && t6.contains("30") && t6.contains("50") && !t6.contains("20"))
-	//	cout << "Copy constructor corrrectly made tree t3 from tree t. (CORRECT)" << endl;
-	//else
-	//	cout << "Copy constructor incorrrectly made tree t3 from tree t. (INCORRECT)" << endl;	
+	
 #pragma endregion old test stuff	
 	system("pause");
 
@@ -253,33 +135,7 @@ void assemble_tree(){
 
 
 	#pragma region temp_tree_testing
-	//cout << "" << endl; //blank line
-	//cout << "" << endl; //blank line
-	//cout << "" << endl; //blank line
-	//cout << "" << endl; //blank line
-	////printing actual right side of tree
-	//while(t.root != nullptr){
-	//	cout << t.root->index << endl;
- //       cout << t.root->_alpha << endl;
- //       cout << t.root->encode_morse << endl;
- //       cout << t.root->morse_overlay << endl;
-
-	//	t.root = t.root->right;
-	//}
-
-	//cout << "" << endl; //blank line
-	//cout << "" << endl; //blank line
-	//cout << "" << endl; //blank line
-	//cout << "" << endl; //blank line
-	////printing actual left side of tree
-	//while(t.root != nullptr){
-	//	cout << t.root->index << endl;
- //       cout << t.root->_alpha << endl;
- //       cout << t.root->encode_morse << endl;
- //       cout << t.root->morse_overlay << endl;
-
-	//	t.root = t.root->left;
-	//}
+	
 	#pragma endregion temp_tree_testing
 }
 
